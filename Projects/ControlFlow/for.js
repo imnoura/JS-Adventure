c0ef5510e04206cff761a7230dad210a28092f26
Noura Mousavi <imnouramousavi@gmail.com>
2023-07-20 19:17:55 +0330

    (chore): add exercise for.js

---
+++ Projects/ControlFlow/for.js
@@ -0,0 +1,18 @@
+for (let i = 0; i < 5; i++) {
+  console.log("Ciao Mondo", i);
+}
+
+//i will be strated from 1 and finishes at 5
+for (let i = 1; i <= 5; i++) {
+  console.log("Ciao Mondo", i);
+}
+
+//display odd numbers between 1 to 5
+for (let i = 1; i <= 5; i++) {
+  if (i % 2 !== 0) console.log(i);
+}
+
+//start from 5 and go back to 1
+for (let i = 5; i >= 1; i--) {
+  console.log("Ciao Mondo", i);
+}
