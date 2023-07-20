63520925137cb97c4ae20ee7a550e13bad029eec
Noura Mousavi <imnouramousavi@gmail.com>
2023-07-20 18:54:11 +0330

    (chore): add another solution to switch-case.js

--- Projects/ControlFlow/switch-case.js
+++ Projects/ControlFlow/switch-case.js
@@ -12,3 +12,9 @@ switch (role) {
   default:
     console.log("Unknown User");
 }
+
+//implomenting the exact logic usibg if...else statements(which is cleaner and shorter)
+
+if (role === "guest") console.log("Guest User");
+else if (role === "moderator") console.log("Moderator User");
+else console.log("Unknown User");
