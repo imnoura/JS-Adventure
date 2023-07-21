65491b8c86003d81979e44b6a974671a654e27ff
Noura Mousavi <imnouramousavi@gmail.com>
2023-07-21 11:53:10 +0330

    (chore): add exercise break-continue.js

---
+++ Projects/ControlFlow/break-continue.js
@@ -0,0 +1,16 @@
+let i = 0;
+while (i <= 10) {
+  if (i === 5) break;
+  console.log(i);
+  i++;
+}
+
+let x = 0;
+while (x <= 10) {
+  if (x % 2 === 0) {
+    x++;
+    continue;
+  }
+  console.log(x);
+  x++;
+}
