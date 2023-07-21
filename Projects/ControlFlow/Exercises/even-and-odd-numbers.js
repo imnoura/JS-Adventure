f58e9988bb17b35d09b99aeaf4fdd9e55ec83876
Noura Mousavi <imnouramousavi@gmail.com>
2023-07-21 14:27:45 +0330

    (chore): complete the given odd even exercise

--- Projects/ControlFlow/Exercises/even-and-odd-numbers.js
+++ Projects/ControlFlow/Exercises/even-and-odd-numbers.js
@@ -1,6 +1,12 @@
 showNumbers(10);
 
 //solution 1 (better one)
+function showNumbers(limit) {
+  for (let i = 0; i <= limit; i++) {
+    const message = i % 2 === 0 ? "EVEN" : "ODD";
+    console.log(i, message);
+  }
+}
 
 //solution 2
 function showNumbers(limit) {
