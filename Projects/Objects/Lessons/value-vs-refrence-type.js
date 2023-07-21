d85a5f7a9416ee6d93b8637d3f95856810806516
Noura Mousavi <imnouramousavi@gmail.com>
2023-07-21 19:03:40 +0330

    (chore): edit the value vs refrence type lesson

--- Projects/Objects/Lessons/value-vs-refrence-type.js
+++ Projects/Objects/Lessons/value-vs-refrence-type.js
@@ -1,3 +1,8 @@
+//JS:
+//Value(primitive): Are value types. Are copied by their calue. *Types: number, string, boolean, symbol, undefined, null
+
+//Reference(objects): are objects. Are copied by their reference  *Types: object, function, array
+
 let obj = { value: 10 };
 
 function increase(obj) {
