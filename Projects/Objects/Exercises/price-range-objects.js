a8654be294349373c92397ab245b323d9d948fb0
Noura Mousavi <imnouramousavi@gmail.com>
2023-07-21 22:25:06 +0330

    (chore): do the price range exercise

---
+++ Projects/Objects/Exercises/price-range-objects.js
@@ -0,0 +1,7 @@
+let priceRanges = [
+  { label: "$", tooltip: "Inexpensive", minPerPerson: 0, maxPerPerson: 10 },
+  { label: "$$", tooltip: "Moderate", minPerPerson: 11, maxPerPerson: 25 },
+  { label: "$$$", tooltip: "Expensive", minPerPerson: 26, maxPerPerson: 50 },
+];
+
+let restaurants = [{ averagePerPerson: 5 }];
