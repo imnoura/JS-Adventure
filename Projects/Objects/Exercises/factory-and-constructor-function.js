e0c06a16399f54ab52ca39c3973e80a570d7bb94
Noura Mousavi <imnouramousavi@gmail.com>
2023-07-21 21:19:51 +0330

    (chore): do the factory and constructor function exercise

---
+++ Projects/Objects/Exercises/factory-and-constructor-function.js
@@ -0,0 +1,27 @@
+//factory function:
+
+let address1 = createAddress("a", "b", "c");
+
+console.log(address1);
+
+function createAddress(street, city, zipCode) {
+  return {
+    street,
+    city,
+    zipCode,
+  };
+}
+
+//constructor function:
+
+let address = new Address("a", "b", "c");
+
+console.log(address);
+
+function Address(street, city, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
+}
+
+const circle = new Address(1);
