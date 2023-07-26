a143d7b4a2c685a1517cdd334c2494980fc1bc58
imnoura <imnouramousavi@gmail.com>
2023-07-26 19:53:20 +0430

    (chore): edit the count occurrence exercise

--- Projects/Arrays/Exercises/count-occurrences.js
+++ Projects/Arrays/Exercises/count-occurrences.js
@@ -1,21 +1,21 @@
 const numbers = [1, 2, 3, 4, 1];
 
-const count = countOccurrences (numbers, 1);
+const count = countOccurrences(numbers, 1);
 
-console.log (count);
+console.log(count);
 
 function countOccurrences(array, searchElement) {
-//    let count = 0;
-//    for (let element of array)
-//        if (element === searchElement)
-//            count++
-//    return count;
-//}
+  //    let count = 0;
+  //    for (let element of array)
+  //        if (element === searchElement)
+  //            count++
+  //    return count;
+  //}
 
-//The reduce method to achive the same thing with the less code: 
-return array.reduce((accumulator, current) => {
-const occurrence = (current === searchElement) ? 1 : 0
-console.log (accumulator, current, searchElement)
-return accumulator + occurrence
-}, 0);
+  //The reduce method to achive the same thing with the less code:
+  return array.reduce((accumulator, current) => {
+    const occurrence = current === searchElement ? 1 : 0;
+    console.log(accumulator, current, searchElement);
+    return accumulator + occurrence;
+  }, 0);
 }
